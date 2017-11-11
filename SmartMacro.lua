0e600a3eb992c8f550712bb7f8de5929bd4ad5e5
razorgrace <33260421+razorgrace@users.noreply.github.com>
2017-11-11 06:32:54 +0200

    moved all macros into separate addon -- SmartMacroExamples

--- SmartMacro.lua
+++ SmartMacro.lua
@@ -9,20 +9,45 @@ local function EntryPoint()
     local MyMacros = addonNamespace.MyMacros()
     local Frames = addonNamespace.Frames(MyMacros)
     local Tooltips = addonNamespace.Tooltips(Frames)
-    
-    addonNamespace.Integration.BlizzardUI(Tooltips)
-    addonNamespace.Integration.ButtonForge(true, Tooltips)
 
-    local Main = addonNamespace.Main(Database, Frames, MyMacros)
+    return {
+        OnStart = function ()
+            addonNamespace.Integration.BlizzardUI(Tooltips)
+            addonNamespace.Integration.ButtonForge(true, Tooltips)
+
+            local Main = addonNamespace.Main(Database, Frames, MyMacros)
+        end,
+
+        RegisterExternalItem = function (uniqueId, item)
+            local var = 'SmartMacroSaved'
+
+            if not _G[var].externals then
+                _G[var].externals = {}
+            end
+
+            if not _G[var].externals[uniqueId] then
+                _G[var].externals[uniqueId] = MyMacros.GetNextId()
+            end
+
+            item.id = _G[var].externals[uniqueId]
+
+            MyMacros.Add(item)
+        end
+    }
 end
 
 local frame = CreateFrame('FRAME', nil, UIParent)
 
+frame:RegisterEvent('ADDON_LOADED')
 frame:RegisterEvent('PLAYER_ENTERING_WORLD')
 
 frame:SetScript('OnEvent', function (self, event, arg1)
+    if event == 'ADDON_LOADED' and arg1 == addonName then
+        _G[addonName] = EntryPoint()
+    end
+
     if event == 'PLAYER_ENTERING_WORLD' then
         self:SetScript('OnEvent', nil)
-        EntryPoint()
+        _G[addonName].OnStart()
     end
 end)
